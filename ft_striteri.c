8a47771ac24b64380cccd7dfb91307e9eb9fd5dd
anth <anton.kolomiets98@gmail.com>
2025-10-13 18:38:33 +0200

    new funcs

---
+++ ft_striteri.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akolomii <akolomii@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 13:34:56 by akolomii          #+#    #+#             */
+/*   Updated: 2025/10/13 18:27:15 by akolomii         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char const *s, void (*f)(unsigned int i, char*))
+{
+	unsigned int	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		(*f)(i, (char *)s + i);
+		i++;
+	}
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: akolomii <akolomii@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 09:35:25 by akolomii          #+#    #+#             */
-/*   Updated: 2025/10/03 09:35:27 by akolomii         ###   ########.fr       */
+/*   Updated: 2025/10/13 18:34:50 by akolomii         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,9 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-//	ft_striteri
-//	ft_putchar_fd
-//	ft_putstr_fd
+void ft_striteri(char const *s, void (*f)(unsigned int i, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 //	ft_putendl_fd
 //	ft_putnbr_fd
 #endif
